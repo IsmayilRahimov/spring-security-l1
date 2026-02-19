0a6a25a6d6f97b641d4d6bb62e3854665e6aad94
IsmayilRahimov <rehimovismayil650@gmail.com>
2026-02-20 00:36:56 +0400

    repository

---
+++ src/main/java/com/az/tehsilly/springsecurityl1/repository/AuthRepository.java
@@ -0,0 +1,15 @@
+package com.az.tehsilly.springsecurityl1.repository;
+
+import com.az.tehsilly.springsecurityl1.entity.AuthEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AuthRepository extends JpaRepository<AuthEntity, Long> {
+
+    Optional<AuthEntity> findByUsername(String username);
+
+    Optional<AuthEntity> findByEmail(String email);
+}
