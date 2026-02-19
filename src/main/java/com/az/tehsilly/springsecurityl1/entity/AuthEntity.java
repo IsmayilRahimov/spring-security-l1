dede97b9d3cb0ad50b081549fede72f471903221
IsmayilRahimov <rehimovismayil650@gmail.com>
2026-02-19 23:54:10 +0400

    entity

--- src/main/java/com/az/tehsilly/springsecurityl1/entity/AuthEntity.java
+++ src/main/java/com/az/tehsilly/springsecurityl1/entity/AuthEntity.java
@@ -1,5 +1,27 @@
 package com.az.tehsilly.springsecurityl1.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
 public class AuthEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String username;
+
+    String email;
+
+    String password;
 }
