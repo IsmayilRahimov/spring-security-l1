bdac5f86a5a5ade2d08fbfe2de197789a302a99d
IsmayilRahimov <rehimovismayil650@gmail.com>
2026-02-21 23:34:29 +0400

    login reguest

---
+++ src/main/java/com/az/tehsilly/springsecurityl1/dto/request/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.az.tehsilly.springsecurityl1.dto.request;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class LoginRequest {
+
+    String email;
+
+    String password;
+}
